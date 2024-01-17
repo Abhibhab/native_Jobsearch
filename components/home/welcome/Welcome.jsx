402b46ed2f58d8691414c2a222b49a74b720e1cc
Abhibhab <abhibhab2001@gmail.com>
2024-01-18 00:46:58 +0530

    welcome-page and job-search

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,65 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './welcome.style'
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
+import { useRouter } from "expo-router";
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 const Welcome = () => {
+  const [activeJobType, setactiveJobType] = useState("Full-time");
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello abhibhab</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job </Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          ></TextInput>
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setactiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          //react maps
+          keyExtractor={(item) => item}
+          contentContainerStyle={{columnGap:SIZES.small}}
+          horizontal
 
-export default Welcome
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
